9ce76540430bea88de6ce571c64475fc8796ebad
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-10-08 15:41:46 +0530

    Practice

---
+++ 01_Assignment/Student_Management_System/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Student_Management_System
+{
+    partial class frm_Collage_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Age = new System.Windows.Forms.Label();
+            this.lbl_Gender = new System.Windows.Forms.Label();
+            this.lbl_Standard = new System.Windows.Forms.Label();
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.tb_Age = new System.Windows.Forms.TextBox();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tb_Standard = new System.Windows.Forms.TextBox();
+            this.cmb_Gender = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Lucida Fax", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.Location = new System.Drawing.Point(162, 84);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(97, 33);
+            this.lbl_Name.TabIndex = 0;
+            this.lbl_Name.Text = "Name";
+            // 
+            // lbl_Age
+            // 
+            this.lbl_Age.AutoSize = true;
+            this.lbl_Age.Font = new System.Drawing.Font("Lucida Fax", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Age.Location = new System.Drawing.Point(162, 171);
+            this.lbl_Age.Name = "lbl_Age";
+            this.lbl_Age.Size = new System.Drawing.Size(69, 33);
+            this.lbl_Age.TabIndex = 1;
+            this.lbl_Age.Text = "Age";
+            // 
+            // lbl_Gender
+            // 
+            this.lbl_Gender.AutoSize = true;
+            this.lbl_Gender.Font = new System.Drawing.Font("Lucida Fax", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Gender.Location = new System.Drawing.Point(162, 256);
+            this.lbl_Gender.Name = "lbl_Gender";
+            this.lbl_Gender.Size = new System.Drawing.Size(120, 33);
+            this.lbl_Gender.TabIndex = 2;
+            this.lbl_Gender.Text = "Gender";
+            // 
+            // lbl_Standard
+            // 
+            this.lbl_Standard.AutoSize = true;
+            this.lbl_Standard.Font = new System.Drawing.Font("Lucida Fax", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Standard.Location = new System.Drawing.Point(162, 344);
+            this.lbl_Standard.Name = "lbl_Standard";
+            this.lbl_Standard.Size = new System.Drawing.Size(145, 33);
+            this.lbl_Standard.TabIndex = 3;
+            this.lbl_Standard.Text = "Standard";
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Name.Location = new System.Drawing.Point(428, 81);
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(271, 36);
+            this.tb_Name.TabIndex = 4;
+            // 
+            // tb_Age
+            // 
+            this.tb_Age.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Age.Location = new System.Drawing.Point(428, 168);
+            this.tb_Age.Name = "tb_Age";
+            this.tb_Age.Size = new System.Drawing.Size(271, 36);
+            this.tb_Age.TabIndex = 5;
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Reset.Location = new System.Drawing.Point(133, 424);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(126, 44);
+            this.btn_Reset.TabIndex = 8;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Insert.Location = new System.Drawing.Point(331, 424);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(128, 44);
+            this.btn_Insert.TabIndex = 9;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.Location = new System.Drawing.Point(537, 424);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(141, 44);
+            this.btn_Update.TabIndex = 10;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.Location = new System.Drawing.Point(750, 424);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(122, 44);
+            this.btn_Delete.TabIndex = 11;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 494);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(872, 196);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // tb_Standard
+            // 
+            this.tb_Standard.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Standard.Location = new System.Drawing.Point(428, 344);
+            this.tb_Standard.Name = "tb_Standard";
+            this.tb_Standard.Size = new System.Drawing.Size(271, 36);
+            this.tb_Standard.TabIndex = 7;
+            // 
+            // cmb_Gender
+            // 
+            this.cmb_Gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Gender.FormattingEnabled = true;
+            this.cmb_Gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmb_Gender.Location = new System.Drawing.Point(428, 256);
+            this.cmb_Gender.Name = "cmb_Gender";
+            this.cmb_Gender.Size = new System.Drawing.Size(271, 37);
+            this.cmb_Gender.TabIndex = 13;
+            // 
+            // frm_Collage_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 702);
+            this.Controls.Add(this.cmb_Gender);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.tb_Standard);
+            this.Controls.Add(this.tb_Age);
+            this.Controls.Add(this.tb_Name);
+            this.Controls.Add(this.lbl_Standard);
+            this.Controls.Add(this.lbl_Gender);
+            this.Controls.Add(this.lbl_Age);
+            this.Controls.Add(this.lbl_Name);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Collage_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Form";
+            this.Load += new System.EventHandler(this.frm_Collage_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Age;
+        private System.Windows.Forms.Label lbl_Gender;
+        private System.Windows.Forms.Label lbl_Standard;
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.TextBox tb_Age;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tb_Standard;
+        private System.Windows.Forms.ComboBox cmb_Gender;
+    }
+}
+
---
+++ 01_Assignment/Student_Management_System/Form1.cs
@@ -0,0 +1,129 @@
+﻿using Student_Management_System.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Management_System
+{
+    public partial class frm_Collage_Form : Form
+    {
+        CollageEntities DB = new CollageEntities();
+        Student Std = new Student();
+
+        int N_ID = 0;
+
+        public frm_Collage_Form()
+        {
+            InitializeComponent();
+            Data_Show();
+        }
+
+        void Clear_Controls()
+        {
+            tb_Name.Clear();
+            tb_Age.Clear();
+            tb_Standard.Clear();
+            cmb_Gender.SelectedIndex = -1;
+        }
+        void Data_Show()
+        {
+            dataGridView1.DataSource = DB.Students.ToList<Student>();
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            Std.Name = tb_Name.Text;
+            Std.Age = Convert.ToInt32( tb_Age.Text);
+            Std.Gender = cmb_Gender.SelectedItem.ToString();
+            Std.Standard = Convert.ToInt32( tb_Standard.Text);
+
+            DB.Students.Add(Std);
+
+           int Res =  DB.SaveChanges();
+
+            if(Res > 0)
+            {
+                MessageBox.Show("Data Inserted Successfully!!!", "", MessageBoxButtons.OK);
+                Data_Show();
+            }
+            else
+            {
+                MessageBox.Show("Data Inserted Failed");
+            }
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            Clear_Controls();
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            Std.Name = tb_Name.Text;
+            Std.Age = Convert.ToInt32(tb_Age.Text);
+            Std.Gender = cmb_Gender.SelectedItem.ToString();
+            Std.Standard = Convert.ToInt32(tb_Standard.Text);
+
+            DB.Entry(Std).State = System.Data.Entity.EntityState.Modified;
+
+            int Res = DB.SaveChanges();
+
+            if (Res > 0)
+            {
+                MessageBox.Show("Data Updated Successfully");
+                Data_Show();
+            }
+            else
+            {
+                MessageBox.Show("Data Updated Failed");
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            DialogResult Dr = MessageBox.Show("Are Your Sure To Delete Data ", "", MessageBoxButtons.YesNo);
+            
+            if(Dr == DialogResult.Yes)
+            {
+                Std = DB.Students.Where(X => X.ID == N_ID).FirstOrDefault();
+                DB.Entry(Std).State = System.Data.Entity.EntityState.Deleted;
+
+                int Res = DB.SaveChanges();
+
+                if (Res > 0)
+                {
+                    MessageBox.Show("Data Deleted Successfully");
+                    Data_Show();
+                }
+                else
+                {
+                    MessageBox.Show("Data Deleltd Failed");
+                }
+
+            }
+        }
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            N_ID = Convert.ToInt32( dataGridView1.CurrentRow.Cells[0].Value);
+
+            Std = DB.Students.Where(X => X.ID == N_ID).FirstOrDefault();
+
+            tb_Name.Text = Std.Name;
+            tb_Age.Text = Convert.ToString( Std.Age);
+            cmb_Gender.SelectedItem = Std.Gender;
+            tb_Standard.Text = Convert.ToString ( Std.Standard);
+            
+        }
+
+        private void frm_Collage_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
